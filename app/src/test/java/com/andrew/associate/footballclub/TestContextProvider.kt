9defe88da5701642cef6e4f217cc4f99521c1890
Andrewtio <andrewprasetio@gmail.com>
2018-11-28 17:29:40 +0700

    Penambahan Testing

---
+++ app/src/test/java/com/andrew/associate/footballclub/TestContextProvider.kt
@@ -0,0 +1,11 @@
+package com.andrew.associate.footballclub
+
+import com.andrew.associate.footballclub.util.CoroutineContextProvider
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlin.coroutines.CoroutineContext
+
+class TestContextProvider : CoroutineContextProvider(){
+    @ExperimentalCoroutinesApi
+    override val main: CoroutineContext = Dispatchers.Unconfined
+}
